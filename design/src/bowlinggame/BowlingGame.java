32530a92c87a29baed9e5ea6650c3f514727664d
v0g004h <vikash.gautam@walmartlabs.com>
2019-12-17 21:20:54 +0530

    refactor

--- design/src/bowlinggame/BowlingGame.java
+++ design/src/bowlinggame/BowlingGame.java
@@ -1,42 +1,37 @@
 /**
- * 
+ *
  */
 package bowlinggame;
 
-/**
- * @author Vikash Kumar
- *
- */
 public class BowlingGame {
 
-	int roll[];
-	int score;
-	int curr_Roll;
+  int roll[];
+  int score;
+  int curr_Roll;
 
-	public void BowlingGame() {
-		roll = new int[21];
-		score = 0;
-	}
+  public void BowlingGame() {
+    roll = new int[21];
+  }
 
-	public void roll(int ballsdown) {
-		this.roll[curr_Roll++] = ballsdown;
-	}
+  public void roll(int ballsDown) {
+    this.roll[curr_Roll++] = ballsDown;
+  }
 
-	public int getScore() {
-		for (int frame = 0; frame < 10; frame++) {
-			if (roll[frame] == 10) {
-				score += 10 + roll[frame + 1] + roll[frame + 2];
-				frame++;
-			} else if (roll[frame] + roll[frame + 1] == 10) {
-				score += 10 + roll[frame + 2];
-				frame += 2;
-			} else {
-				score += roll[frame] + roll[frame + 1];
-				frame += 2;
-			}
-		}
+  public int getScore() {
+    for (int frame = 0; frame < 10; frame++) {
+      if (roll[frame] == 10) {
+        score += 10 + roll[frame + 1] + roll[frame + 2];
+        frame++;
+      } else if (roll[frame] + roll[frame + 1] == 10) {
+        score += 10 + roll[frame + 2];
+        frame += 2;
+      } else {
+        score += roll[frame] + roll[frame + 1];
+        frame += 2;
+      }
+    }
 
-		return score;
-	}
+    return score;
+  }
 
 }
