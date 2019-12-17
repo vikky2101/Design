edb8b0c42ab58ac77552d3252b3521af316a02f4
v0g004h <vikash.gautam@walmartlabs.com>
2019-12-17 20:29:05 +0530

    refactor

--- design/src/lrucache/LRUCache.java
+++ design/src/lrucache/LRUCache.java
@@ -3,17 +3,18 @@ package lrucache;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
-public class LRUCache<K,V> extends LinkedHashMap<K, V> {
-	private final int cache_Size;
-	
-	public LRUCache(final int size) {
-		super(size, 0.75F, true);
-		this.cache_Size = size;
-	}
-	
-	@Override
-	protected boolean removeEldestEntry(Map.Entry<K, V> eldest){
-		return this.cache_Size > 10;
-	}
+public class LRUCache<K, V> extends LinkedHashMap<K, V> {
+
+  private final int cache_Size;
+
+  public LRUCache(final int size) {
+    super(size, 0.75F, true);
+    this.cache_Size = size;
+  }
+
+  @Override
+  protected boolean removeEldestEntry(Map.Entry<K, V> eldest) {
+    return this.cache_Size > 10;
+  }
 
 }
